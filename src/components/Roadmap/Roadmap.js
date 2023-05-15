3e57c060d5ff21c0dc6f708207bb6644e291da6d
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-05-16 00:18:43 +0530

    new

--- src/components/Roadmap/Roadmap.js
+++ src/components/Roadmap/Roadmap.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import './Roadmap.css'
 import Line from '../../images/line2.png'
-// import RoadmapImg from '../../images/roadmap-connector.png'
-import Road from '../../images/road.png'
+
+import Road from '../../images/road1.png'
+import Road2 from '../../images/road2.png'
+import Road3 from '../../images/road3.png'
+import Road4 from '../../images/road4.png'
+import Road5 from '../../images/road5.png'
 
 function Roadmap() {
   return (
@@ -16,148 +20,34 @@ function Roadmap() {
         </div>
 
         <div className='container text-center pb-5 pt-5 '>
-          <img  src={Road} alt="road" className='road-img-1'/>
+          <div className='row items-align-center justify-content-center'>
+            <div className='col-md-4 position-relative'>
+            <img  src={Road} alt="road" className='road-img-1'/>
+
+            </div>
+            <div className='col-md-4'>
+            <img  src={Road2} alt="road" className='road-img-1'/>
+            </div>
+
+            <div className='col-md-4'>
+            <img  src={Road3} alt="road" className='road-img-1'/>
+            </div>
+
+            <div className='col-md-6'>
+            <img  src={Road3} alt="road" className='road-img-2'/>
+            </div>
+
+            <div className='col-md-6'>
+            <img  src={Road3} alt="road" className='road-img-2'/>
+            </div>
+
+
+
+          </div>
+        
+
         </div>
-      {/* <main className='roadmap-road'>
-
-      <div className="left-roadmap">
-              <div className="roadmap-card">
-                <div className="roadmap-mobile-line"></div>
-                <div>
-                  <p className="roadmap-date weight-7">Phase 1. 2022 </p>
-                  <img
-                    src={RoadmapImg}
-                    className="roadmap-connector"
-                    alt=""
-                  />
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    1. Launch on BSC mainnet 
-                  </p>
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    2. 29th August - Private sale for 10 days only 
-                  </p>
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    3. Smart contract security audit 
-                  </p>
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    4.  Pre-sale for 20 days 
-                  </p>
-                </div>
-              </div>
-              <div className="roadmap-card">
-                <div className="roadmap-mobile-line"></div>
-                <div className="road-mob-2">
-                  <p className="roadmap-date weight-7">Phase 3 2023 
-</p>
-                  <img
-                    src={RoadmapImg}
-                    className="roadmap-connector"
-                    alt=""
-                  />
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    1. Staking for KBT and other cryptos - January 2023
-
-                  </p>
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    2. Forex Trading features in the platform - February 2023
-
-                  </p>
-                </div>
-
-               
-                <div className="road-mob">
-                  <p className="roadmap-date weight-7">Phase 2 2022 
-</p>
-                  <img
-                    src={RoadmapImg}
-                    className="roadmap-connector"
-                    alt=""
-                  />
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    1. Listing cmc and CG 
-                  </p>
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    2. Strong marketing strategy for growth 
-                  </p>
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    3. Platform Beta version launch - November 2022
-                  </p>
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    4. Platform Public launch  other famous coin listing websites - Listing of token- December 2022
-                  </p>
-                </div>
-              </div>
-      
- 
-            </div>
-            <div className="right-roadmap">
-              <div className="roadmap-card">
-                <div className="roadmap-mobile-line"></div>
-                <div className="road-mob-2">
-                  <p className="roadmap-date weight-7">Phase 2 2022 
-</p>
-                  <img
-                    src={RoadmapImg}
-                    className="roadmap-connector"
-                    alt=""
-                  />
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    1. Listing cmc and CG 
-                  </p>
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    2. Strong marketing strategy for growth 
-                  </p>
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    3. Platform Beta version launch - November 2022
-                  </p>
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    4. Platform Public launch  other famous coin listing websites - Listing of token- December 2022
-                  </p>
-                </div>
-                <div className="road-mob">
-                  <p className="roadmap-date weight-7">Phase 3 2023 
-</p>
-                  <img
-                    src={RoadmapImg}
-                    className="roadmap-connector"
-                    alt=""
-                  />
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    1. Staking for KBT and other cryptos - January 2023
-
-                  </p>
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    2. Forex Trading features in the platform - February 2023
-
-                  </p>
-                </div>
-              </div>
-              <div className="roadmap-card">
-                <div className="roadmap-mobile-line"></div>
-                <div>
-                  <p className="roadmap-date weight-7">Phase-4 2023 
-</p>
-                  <img
-                    src={RoadmapImg}
-                    className="roadmap-connector"
-                    alt=""
-                  />
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    1. P2P Trading - March 2023
-                  </p>
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    2. Swap features - April 2023
-                  </p>
-                  <p className="roadmap-card-title weight-4 lh-1_7">
-                    3. Mobile apps - May 2023
-                  </p>
-                </div>
-              </div>
-            
-
-       
-            </div>
-      </main> */}
+   
       </div>
 
 
